137709ee335240fb36e7875f5f32196db956d5ce
lior grossman <lior.grosman@gmail.com>
2017-01-21 23:47:54 +0200

    ok

--- AngulrJs2/AngulrJs2/Controllers/LockHub.cs
+++ AngulrJs2/AngulrJs2/Controllers/LockHub.cs
@@ -6,29 +6,41 @@ using System.Threading;
 using System.Web.Http;
 using Elal.FlightPostponed.DataModel.ViewModel;
 using System.Collections.Generic;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace Elal.FlightPostponed.MobileWeb.Controllers
 {
     #region LockHub
+    [HubName("LockHub")]
     public class LockHub : Hub
     {
-        public async Task Subscribe(string channel)
-        {
-            await Groups.Add(Context.ConnectionId, channel);
+        //public async Task Subscribe(string channel)
+        //{
+        //    await Groups.Add(Context.ConnectionId, channel);
 
+        //    var ev = new SessionStatus();
+        //    ev.Users = new List<UserHandle>();
+        //    ev.State = (int)StateSignalREnum.Sub;
+        //    await Publish(ev);
+        //}
+
+        public async Task Ping()
+        {
             var ev = new SessionStatus();
+        
             ev.Users = new List<UserHandle>();
             ev.State = (int)StateSignalREnum.Sub;
-            await Publish(ev);
+            await Publish(ev,"ping");
         }
 
 
-        public Task Publish(SessionStatus channelEvent)
+        public Task Publish(SessionStatus channelEvent,string channel="")
         {
-            Clients.Group(SessionStatus.TaskChannel).OnEvent(SessionStatus.TaskChannel, channelEvent);
+            var c = String.IsNullOrEmpty(channel) ? SessionStatus.TaskChannel : channel;
+            Clients.All.GetNotify(c, channelEvent);
+            //  Clients.Group(SessionStatus.TaskChannel).GetNotify(SessionStatus.TaskChannel, channelEvent);
             return Task.FromResult(0);
         }
-
 
         public override Task OnConnected()
         {
@@ -49,18 +61,17 @@ namespace Elal.FlightPostponed.MobileWeb.Controllers
             ev.State = (int)StateSignalREnum.UnSub;
 
             Publish(ev);
-             
+
             return base.OnDisconnected(stopCalled);
         }
 
     }
 }
-    #endregion
+#endregion
 
-   
 
-   
- 
-    
 
-  
+
+
+
+
